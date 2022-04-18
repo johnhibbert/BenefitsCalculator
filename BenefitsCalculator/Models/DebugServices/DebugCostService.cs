72e52182dde3e9902cf2d87deaebd21dd94bcbf2
johnhibbert <johnhibbert@users.noreply.github.com>
2022-04-18 14:55:41 -0400

    Adding files removed in error by Git Ignore

---
+++ BenefitsCalculator/Models/DebugServices/DebugCostService.cs
@@ -0,0 +1,28 @@
+﻿using BenefitsCalculator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenefitsCalculator.Services.Debug
+{
+    //I guess you might also call this a "Mock"
+    public class DebugCostService : IBenefitCostService
+    {
+        public double GetDiscountForNameStartingWithA()
+        {
+            return 0.1;
+        }
+
+        public double GetYearlyBenefitCost()
+        {
+            return 1000.0;
+        }
+
+        public double GetYearlyDependentCost()
+        {
+            return 500;
+        }
+    }
+}
---
+++ BenefitsCalculator/Models/DebugServices/DebugPayrollService.cs
@@ -0,0 +1,23 @@
+﻿using BenefitsCalculator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenefitsCalculator.Services.Debug
+{
+    //I guess you might also call this a "Mock"
+    public class DebugPayrollService : IPayrollService
+    {
+        public double GetGrossDollarAmountPerEmployeePaycheck()
+        {
+            return 2000.0;
+        }
+
+        public int GetNumberOfPaychecksPerYear()
+        {
+            return 26;
+        }
+    }
+}
